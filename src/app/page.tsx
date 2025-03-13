b9cb789cdd679cdca68528042a8440235a27f0f3
rainzoneg <samuelnathaniel1234@gmail.com>
2025-03-13 23:53:45 +0700

    improv: styling adjustments, clear message functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-fuchsia-950/15 flex items-center justify-center min-h-screen font-[family-name:var(--font-geist-sans)]">
+    <div className="bg-stone-950 flex items-center justify-center min-h-screen font-[family-name:var(--font-geist-sans)]">
       <div className="w-full flex items-center justify-center">
         <ChatInterface />
       </div>
--- src/component/ChatInterface/ChatInterface.tsx
+++ src/component/ChatInterface/ChatInterface.tsx
@@ -30,13 +30,17 @@ export default function ChatInterface() {
 
     try {
       setIsLoading(true);
-      
+
       const updatedMessages = [
         ...messages,
-        { message: userMsg, sender: "user", timestamp: new Date().toISOString() },
+        {
+          message: userMsg,
+          sender: "user",
+          timestamp: new Date().toISOString(),
+        },
       ];
       const response = await generateAIResponse(userMsg, updatedMessages);
-      
+
       if (response) {
         setMessages((prevMessages) => [
           ...prevMessages,
@@ -72,16 +76,16 @@ export default function ChatInterface() {
   };
 
   return (
-    <div className="flex flex-col gap-5 w-2/5">
-      <div className="flex flex-col h-[65vh] overflow-y-auto w-full bg-blue-950/15 rounded-lg p-6 border-1 border-white">
-        <div className="flex flex-col flex-grow">
+    <div className="flex flex-col gap-5 w-4/5 md:w-3/5 lg:w-2/5">
+      <div className="flex flex-col h-[65vh] overflow-y-auto w-full bg-blue-950/30 rounded-lg px-6 pt-6 pb-1 border-1 border-white">
+        <div className="flex flex-col">
           {messages.length >= 1 ? (
-            <>
+            <div className="flex flex-col gap-5">
               {messages.map((message, index) => {
                 return (
                   <div
                     key={index}
-                    className="flex flex-col mt-4 bg-amber-50 text-black px-4 py-2 rounded-lg my-1 break-words"
+                    className="flex flex-col bg-amber-50 text-black px-4 py-2 rounded-lg break-words"
                   >
                     <p
                       className={
@@ -100,7 +104,7 @@ export default function ChatInterface() {
                 );
               })}
               <div ref={messagesEndRef} />
-            </>
+            </div>
           ) : (
             <p className="text-center my-auto">Type your message to begin!</p>
           )}
@@ -109,21 +113,32 @@ export default function ChatInterface() {
 
       <form onSubmit={handleSubmit}>
         <div className="flex flex-col gap-5">
-          <input
-            className="flex flex-col h-[8vh] w-full bg-white text-black px-6 rounded-lg"
-            type="text"
-            placeholder="Type your message here..."
-            onChange={(e) => setMessage(e.target.value)}
-            value={message}
-            disabled={isLoading}
-          />
+          <div className="relative">
+            <input
+              className="flex flex-col h-[8vh] w-full bg-white text-black px-6 rounded-lg"
+              type="text"
+              placeholder="Type your message here..."
+              onChange={(e) => setMessage(e.target.value)}
+              value={message}
+              disabled={isLoading}
+            />
+            {message.length > 0 && (
+              <div
+                className="absolute right-4 top-1/2 -translate-y-1/2 flex items-center justify-center rounded-full w-6 h-6 bg-gray-200 transition-colors duration-200 hover:bg-red-400 hover:cursor-pointer"
+                onClick={() => setMessage("")}
+                aria-label="Clear message"
+              >
+                X
+              </div>
+            )}
+          </div>
 
           <button
-            className="bg-blue-200 w-full py-4 rounded-lg text-black hover:bg-red-200 hover:text-white"
+            className="bg-blue-200 w-full py-4 rounded-lg text-black transition-colors duration-150 hover:bg-red-200 hover:text-white hover:cursor-pointer"
             type="submit"
             disabled={isLoading}
           >
-            <div>{isLoading ? "Generating response..." : "Send"}</div>
+            <p>{isLoading ? "Generating response..." : "Send"}</p>
           </button>
         </div>
       </form>
